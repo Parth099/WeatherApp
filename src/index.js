fdd2af755ce7933bd647b65cee42a5b96f73d761
Parth099 <parthnpatel715@gmail.com>
2021-11-20 23:02:53 -0500

    API init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import "./styles.css";
-const openWeatherAPIKey = "886705b4c1182eb1c69f28eb8c520e20"; //to be updated
+import "./reset.css";
 
+const openWeatherAPIKey = "d46b5662af879b27eb72e6846556115c"; //oh no
+let cityName = "Philadelphia"; //default
+let cityWeatherData;
 
+async function getWeatherJson() {
+  const openWeatherReponse = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${openWeatherAPIKey}`);
+  const openWeatherReponseJSON = await openWeatherReponse.json();
+  if (openWeatherReponseJSON.cod == 200) {
+    console.log(openWeatherReponseJSON.cod);
+    cityWeatherData = openWeatherReponseJSON;
+  }
+  console.log(cityWeatherData);
+}
+getWeatherJson();
